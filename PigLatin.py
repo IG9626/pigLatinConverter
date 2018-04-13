ef7110e2d868e3b69df3436f44ef7c64db1c8248
IG9626 <31745226+IG9626@users.noreply.github.com>
2018-04-13 13:51:48 -0600

    Add files via upload

---
+++ PigLatin.py
@@ -0,0 +1,109 @@
+a='a'
+b='b'
+c='c'
+d='d'
+e='e'
+f='f'
+g='g'
+h='h'
+i='i'
+j='j'
+k='k'
+l='l'
+m='m'
+n='n'
+o='o'
+p='p'
+q='q'
+r='r'
+s='s'
+t='t'
+u='u'
+v='v'
+w='w'
+x='x'
+y='y'
+z='z'
+
+word = input("Enter word:")
+wordl = word.lower()
+if wordl[0] == a:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('vowel')
+elif wordl[0] ==b:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] ==c:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] ==d:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] == e:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('vowel')
+elif wordl[0] ==f:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] ==g:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] ==h:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] == i:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('vowel')
+elif wordl[0] ==j:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] ==k:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] ==l:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] ==m:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] ==n:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] == o:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('vowel')
+elif wordl[0] ==p:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] ==q:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] ==r:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] ==s:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] ==t:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] == u:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('vowel')
+elif wordl[0] ==v:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] ==w:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] ==x:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+elif wordl[0] == y:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('vowel')
+elif wordl[0] ==z:
+    print(wordl[1:]+wordl[0]+'ay')
+    print('consonant')
+    
+
